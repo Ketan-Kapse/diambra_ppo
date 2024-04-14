f39870083e12d6ae252ec7782d6d56bef47ff65f
Ketan Kapse <ketanckapse@gmail.com>
2024-04-14 18:24:20 -0400

    Add files via upload

--- agent.py
+++ agent.py
@@ -36,7 +36,7 @@ def main(cfg_file, trained_model, test=True):
 
     # Load the trained agent
     model_path = os.path.join(model_folder, trained_model)
-    agent = PPO.load('models.zip', env=env)
+    agent = PPO.load('models/models.zip', env=env)
 
     # Print policy network architecture
     print("Policy architecture:")
@@ -63,7 +63,7 @@ def main(cfg_file, trained_model, test=True):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--cfgFile", type=str, default = 'config.yaml', help="Configuration file")
-    parser.add_argument("--trainedModel", type=str, default="models.zip", help="Model checkpoint")
+    parser.add_argument("--trainedModel", type=str, default="models", help="Model checkpoint")
     parser.add_argument("--test", type=int, default=0, help="Test mode")
     opt = parser.parse_args()
     print(opt)
